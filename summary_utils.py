b8b6541cc98ac5ecb47d6079c80f412dc489b756
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-07-10 14:49:29 +0530

    Update summary_utils.py

--- summary_utils.py
+++ summary_utils.py
@@ -57,5 +57,4 @@ def parse_tradebook(file) -> pd.DataFrame:
     summary['Avg_Price'] = summary['Avg_Price'].round(2)
     summary['Total_Value'] = summary['Total_Value'].round(2)
     
-    uploaded_file.seek(0)
     return summary
