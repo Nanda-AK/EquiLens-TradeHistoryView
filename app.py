867bbffe661cb5ef512cb31aacb028aba8972072
Nanda-AK <59553626+Nanda-AK@users.noreply.github.com>
2025-07-10 14:47:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,10 +33,9 @@ with st.container():
         # --- PROCESS CSV ---
     if uploaded_file:
         try:
+            summary_df = parse_tradebook(uploaded_file)
             df = pd.read_csv(uploaded_file)
             oldest_date, latest_date = get_trade_date_range(uploaded_file)
-            summary_df = parse_tradebook(uploaded_file)
-            
     
             # Recommended layout: center the table using Streamlit's grid
             left, center, right = st.columns([1, 6, 1])
